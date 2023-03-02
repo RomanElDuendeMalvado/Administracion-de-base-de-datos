becac3cf8da780df91e65709a47173ba5e365713
unknown <manuelalejandropereiraroman@gimnasiocalibio.edu.co>
2023-03-02 11:41:30 -0500

    agrego SQL

---
+++ Administracion de datos.sql
@@ -0,0 +1,36 @@
+create database Noveno;
+drop database Noveno;
+create database Calibio;
+Use Calibio;
+create table Estudiante(
+Nombre varchar(50) NOT NULL,
+Apellido varchar(50) NOT NULL,
+ID Numeric(10) NOT NULL Primary key);
+insert into Estudiante Values(
+"Manuel",
+"Roman",
+2007);
+insert into Estudiante Values(
+"Victor",
+"Phasmo",
+2009);
+insert into Estudiante Values(
+"Nicolas",
+"Argonas",
+2008);
+insert into Estudiante Values(
+"Sebastian",
+"Rosero",
+2009);
+insert into Estudiante Values(
+"Santiago",
+"Gonzales",
+2008);
+insert into Estudiante Values(
+"Juanfe",
+"Herrera",
+2006);
+insert into Estudiante Values(
+"Juan Manuel",
+"Figueroa",
+2010);
